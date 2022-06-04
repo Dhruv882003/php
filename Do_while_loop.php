813ffb8e7f98a8e28e19af1063e55c65fed8fbf3
Dhruv882003 <101918092+Dhruv882003@users.noreply.github.com>
2022-06-04 13:30:41 +0530

    Add files via upload

---
+++ Do_while_loop.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <title>while loop</title>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=divice-width, inital-scale=1">
+</head>
+
+<body bgcolor="#e49e41" align="center">
+    <?php
+    echo "<h1>Do_while loop</h1>";
+    echo "Abhishek kumar singh <br>";
+    $a = 0;
+    do {
+        echo $a . "<br>";
+        $a++;
+    } while ($a <= 10);
+    echo "This is the end of the do-while loop";
+    ?>
+</body>
+
+</html>
